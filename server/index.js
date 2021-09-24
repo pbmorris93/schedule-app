9f5d9e955495426acadb484f5ce9a50a268a8a4d
Patrick Morris <patrickbmorris93@gmail.com>
2021-09-23 22:03:09 -0500

    Fixing startup script.

--- server/index.js
+++ server/index.js
@@ -4,10 +4,12 @@ const express = require('express'); // This brings in the express library.
 const app = express(); // assign express to the variable "app"
 const port = process.env.PORT || 3000; // define the port for the server to listen on.
 
+// Define a GET endpoint that returns "Hello world" to the user.
 app.get('/test/', (req, res) => {
     res.send(`Hello world!`);
 });
 
+// Tell the app to listen on the defined port.
 app.listen(port, () => {
     console.log(`API listening!!`);
 });
