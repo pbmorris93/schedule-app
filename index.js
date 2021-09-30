1a09c1246d3554ae9d8629dd21a46a2399a61e7c
Josh <joshsmitherino4@gmail.com>
2021-09-29 22:05:15 -0500

    Adding mongodb dependency. Also connecting and writing information to the cluster.

--- index.js
+++ index.js
@@ -13,3 +13,27 @@ app.get('/test/', (req, res) => {
 app.listen(port, () => {
     console.log(`API listening!!`);
 });
+
+
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://testUser:<INSERT_PASSWORD_HERE>@schedule-app-cluster.tnz19.mongodb.net/schedule-app?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("schedule-app").collection("teams");
+  // perform actions on the collection object
+
+  var sampleTeam = {
+    ID: 1,
+    TeamName: "Sample Team",
+    HomeCity: "Sample Home City",
+    HomeVenueID: 1,
+    HubID: 1
+  };
+
+
+  collection.insertOne(sampleTeam, {w: 1}, function(err, records){
+  console.log("Error: " + err);
+
+  client.close();
+});
+});
